fda9891679613c3c9f5dbed41ceb43cd8ecb8d6c
Srinivas Valekar <srinivasvalekar@Srinivass-MacBook-Pro.local>
2020-01-26 12:28:34 +0000

    added documentation, modified some files, code cleaning, final commit

--- src/routes/FileHandler.ts
+++ src/routes/FileHandler.ts
@@ -1,72 +1,125 @@
-// import mongoose from "mongoose";
-// import Grid from "gridfs-stream";
-// //import logger from "morgan";
-// import { paramMissingError, logger, adminMW } from "@shared";
-// import express, { Router } from "express";
-// import { BAD_REQUEST } from "http-status-codes";
+import { Request, Response, Router } from "express";
 
-// var Busboy = require("busboy");
-// const app1 = express();
+import upload from "../helpers/storageBuilder";
+import { DatabaseError } from "@errors";
+import { adminMW, logger, paramMissingError } from "@shared";
+import { INTERNAL_SERVER_ERROR, BAD_REQUEST, OK } from "http-status-codes";
+import { ParamsDictionary } from "express-serve-static-core";
 
-// //app1.use(busboy({ immediate: true }));
-// app1.use(express.urlencoded({ extended: true }));
+import { connection } from "mongoose";
+const Grid = require("gridfs-stream");
+const mongoose = require("mongoose");
+const mongoURI = process.env.MONGO_URL;
 
-// const router = Router();
-// app1.use("/api", router);
-// /******************************************************************************
-//  *                      upload a photo - "POST /api/photos/upload"
-//  ******************************************************************************/
-// router.post("/upload", async (req: any, res: any) => {
-//   try {
-//     /* const connection = mongoose.createConnection(
-//       process.env.MONGOOSE_CONNECTION_URL + ""
-//     );*/
+const router = Router();
 
-//     var busby = new Busboy({ headers: req.headers });
-//     busby.on(
-//       "file",
-//       (
-//         fieldName: String,
-//         file: any,
-//         fileName: any,
-//         encoding: any,
-//         mine: any
-//       ) => {
-//         let part = file;
-//       }
-//     );
+const openConnection = async () => {
+  try {
+    const connection = mongoose.createConnection(mongoURI);
+    await connection.once("open", () => {});
+    let gfs = Grid(connection.db, mongoose.mongo);
+    gfs.collection("uploads");
+    return [gfs, connection];
+  } catch (err) {
+    throw new DatabaseError(err.message);
+  }
+};
+/******************************************************************************
+ *                      FILE - POST /api/files/
+ ******************************************************************************/
+router.post("/", [adminMW, upload.single("file")], (req: any, res: any) => {
+  res.json({ file: req.file });
+});
 
-//     /* connection.once("open", () => {
-//       var gridfs = Grid(connection.db, mongoose.mongo);
-//       let part = req.files.file;
-//       let writeStream = gridfs.createWriteStream({
-//         filename: "img_" + part.name,
-//         mode: "w",
-//         content_type: part.mimetype
-//       });
+/******************************************************************************
+ *                      FILE - GET ALL /api/files/
+ ******************************************************************************/
+router.get("/", adminMW, async (req: Request, res: Response) => {
+  try {
+    const [gfs, connection] = await openConnection();
+    const files = await gfs.files.find().toArray();
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: "No files exist"
+      });
+    }
+    return res.json(files);
+  } catch (err) {
+    res.status(INTERNAL_SERVER_ERROR).json({
+      err: err.message
+    });
+  } finally {
+    logger.info("Closing MongoDB connection");
+    connection.close();
+  }
+});
 
-//       writeStream.on("close", (file: any) => {
-//         return res.status(200).send({
-//           message: "Success",
-//           file: file
-//         });
-//       });
+/******************************************************************************
+ *                      FILE - GET ONE /api/files/:filename
+ ******************************************************************************/
+router.get("/:filename", adminMW, async (req: Request, res: Response) => {
+  //let connection;
+  try {
+    const { filename } = req.params as ParamsDictionary;
+    if (!filename) {
+      return res.status(BAD_REQUEST).json({
+        error: paramMissingError
+      });
+    }
 
-//       writeStream.write(part.data);
-//       writeStream.end();
-//       connection.close();
-//     });*/
-//     //return res.status(OK).end();
-//   } catch (err) {
-//     logger.error(err.message, err);
-//     return res.status(BAD_REQUEST).json({
-//       error: err.message
-//     });
-//   }
-// });
+    const [gfs, connection] = await openConnection();
 
-// router.get("/test", (req: any, res: any) => {
-//   res.send("Hellow");
-// });
+    const file = await gfs.files.findOne({ filename: filename });
 
-// export default app1;
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: "No file exists"
+      });
+    }
+    if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: "Not an image"
+      });
+    }
+  } catch (err) {
+    res.status(INTERNAL_SERVER_ERROR).json({
+      err: err.message
+    });
+  } finally {
+    logger.info("Closing MongoDB connection");
+    connection.close();
+  }
+});
+
+/******************************************************************************
+ *                      FILE - DELETE ONE /api/files/:filename
+ ******************************************************************************/
+router.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params as ParamsDictionary;
+    if (!id) {
+      return res.status(BAD_REQUEST).json({
+        error: paramMissingError
+      });
+    }
+    const [gfs, connection] = await openConnection();
+    const result = await gfs.remove({
+      _id: req.params.id,
+      root: "uploads"
+    });
+    return res.status(OK).end();
+  } catch (err) {
+    res.status(404).json({
+      err: err.message
+    });
+  } finally {
+    logger.info("Closing MongoDB Connection");
+    connection.close();
+  }
+});
+
+export default router;
